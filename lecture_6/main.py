cb46fd11da278564fda946783743c6e516a1f80b
alfred <alfredhager0420@gmail.com>
2025-12-17 15:24:23 +0300

    v1.0.0

---
+++ lecture_6/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/healthcheck')
+async def healthcheck() -> dict:
+    return {'status' : 'ok'}
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run(
+        app="main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+    )
